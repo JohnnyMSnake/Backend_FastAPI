e1edc84e73354e93f461afe3ae28b8755d326141
Diego Rose <l22550309@chihuahua2.tecnm.mx>
2026-03-02 11:42:54 -0600

    falta ortografica

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,5 @@
 from .services.validacion_service import Validacion_Service
 
-#Aqui se registran todos los services para que peudan ser inyectados
+#Aqui se registran todos los services para que puedan ser inyectados
 def Validacion():
     return Validacion_Service()
