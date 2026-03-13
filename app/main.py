a0e2136d7c25b052415e99d58803f0047ac5fa6c
Diego Rose <l22550309@chihuahua2.tecnm.mx>
2026-03-13 13:15:32 -0600

    configuracion del cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import procesar_controller
 
 app = FastAPI()
+
+# Configuracion del cors
+origins = [
+    "http://localhost:3000",  # frontend en el localhost)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Aqui van registrando los controllers (routers por que aqui los llaman asi (?)) creados, 
 # seguir la mimsa estructura cada que se quiera crear un nuevo controlador
