347f813a55a63a0426746e277915310dd75e6995
Jonathan David Salazar Vazquez <l22550321@chihuahua2.tecnm.mx>
2026-04-07 18:24:17 -0600

    Test para xlsx agregado
    Se agrego Nuevo test para xlsx, fallaba por que estaba corrompido el original

--- tests/unit/test_validacion_service.py
+++ tests/unit/test_validacion_service.py
@@ -10,6 +10,13 @@ def test_validar_archivo_csv_devuelve_dataframe(csv_upload_file):
 	assert len(dataframe) == 5
 	assert {"fecha", "importe", "margen", "nombre_sucursal", "nombre_cliente", "nombre_vendedor", "nombre_linea", "kilogramos"}.issubset(dataframe.columns)
 
+def test_validar_archivo_xlsx_devuelve_dataframe(xlsx_upload_file):
+	service = Validacion_Service()
+	dataframe = service.ValidarArchivo(xlsx_upload_file)
+
+	assert len(dataframe) == 5
+	assert {"fecha", "importe", "margen", "nombre_sucursal", "nombre_cliente", "nombre_vendedor", "nombre_linea", "kilogramos"}.issubset(dataframe.columns)
+
 def test_validar_archivo_formato_no_soportado(unsupported_upload_file):
 	service = Validacion_Service()
 
